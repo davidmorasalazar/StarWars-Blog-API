edd5c3bf9a61d87f2ae11057579d2b1a3296ec95
davidmorasalazar <davidmsalazar99@gmail.com>
2021-03-03 16:52:13 +0000

    my final commit v2

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Favorites
+from models import db, User, Favorites, Characters, Planets
 import datetime
 
 ## Nos permite hacer las encripciones de contraseñas
